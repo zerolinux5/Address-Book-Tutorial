4e71f14a49c20f84b14bc5779c13b8fc7b1746d3
Jesus Magana <maganazero5@gmail.com>
2014-06-28 15:11:12 -0700

    Added address book permissions

--- PetBook/ViewController.m
+++ PetBook/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+@import AddressBook;
 
 @interface ViewController ()
 
@@ -16,6 +17,25 @@
 
 
 - (IBAction)petTapped:(UIButton *)sender {
+    if (ABAddressBookGetAuthorizationStatus() == kABAuthorizationStatusDenied ||
+        ABAddressBookGetAuthorizationStatus() == kABAuthorizationStatusRestricted){
+        //1
+        UIAlertView *cantAddContactAlert = [[UIAlertView alloc] initWithTitle: @"Cannot Add Contact" message: @"You must give the app permission to add the contact first." delegate:nil cancelButtonTitle: @"OK" otherButtonTitles: nil];
+        [cantAddContactAlert show];
+    } else if (ABAddressBookGetAuthorizationStatus() == kABAuthorizationStatusAuthorized){
+        //2
+        UIAlertView *cantAddContactAlert = [[UIAlertView alloc] initWithTitle: @"Cannot Add Contact" message: @"You must give the app permission to add the contact first." delegate:nil cancelButtonTitle: @"OK" otherButtonTitles: nil];
+        [cantAddContactAlert show];
+    } else{ //ABAddressBookGetAuthorizationStatus() == kABAuthorizationStatusNotDetermined
+        //3
+        ABAddressBookRequestAccessWithCompletion(ABAddressBookCreateWithOptions(NULL, nil), ^(bool granted, CFErrorRef error) {
+            if (!granted){
+                //4
+                return;
+            }
+            //5
+        });
+    }
 }
 
 @end
