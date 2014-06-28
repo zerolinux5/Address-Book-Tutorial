ddb1caa5ef99b056bf8a239b913ed170cf476b88
Jesus Magana <maganazero5@gmail.com>
2014-06-28 15:16:22 -0700

    Added logic to adding contacts

--- PetBook/ViewController.m
+++ PetBook/ViewController.m
@@ -24,18 +24,60 @@
         [cantAddContactAlert show];
     } else if (ABAddressBookGetAuthorizationStatus() == kABAuthorizationStatusAuthorized){
         //2
-        UIAlertView *cantAddContactAlert = [[UIAlertView alloc] initWithTitle: @"Cannot Add Contact" message: @"You must give the app permission to add the contact first." delegate:nil cancelButtonTitle: @"OK" otherButtonTitles: nil];
-        [cantAddContactAlert show];
+        [self addPetToContacts:sender];
     } else{ //ABAddressBookGetAuthorizationStatus() == kABAuthorizationStatusNotDetermined
         //3
         ABAddressBookRequestAccessWithCompletion(ABAddressBookCreateWithOptions(NULL, nil), ^(bool granted, CFErrorRef error) {
             if (!granted){
                 //4
+                UIAlertView *cantAddContactAlert = [[UIAlertView alloc] initWithTitle: @"Cannot Add Contact" message: @"You must give the app permission to add the contact first." delegate:nil cancelButtonTitle: @"OK" otherButtonTitles: nil];
+                [cantAddContactAlert show];
                 return;
             }
             //5
+            [self addPetToContacts:sender];
         });
     }
+}
+
+- (void)addPetToContacts: (UIButton *) petButton{
+    NSString *petFirstName;
+    NSString *petLastName;
+    NSString *petPhoneNumber;
+    NSData *petImageData;
+    if (petButton.tag == 1){
+        petFirstName = @"Cheesy";
+        petLastName = @"Cat";
+        petPhoneNumber = @"2015552398";
+        petImageData = UIImageJPEGRepresentation([UIImage imageNamed:@"contact_Cheesy.jpg"], 0.7f);
+    } else if (petButton.tag == 2){
+        petFirstName = @"Freckles";
+        petLastName = @"Dog";
+        petPhoneNumber = @"3331560987";
+        petImageData = UIImageJPEGRepresentation([UIImage imageNamed:@"contact_Freckles.jpg"], 0.7f);
+    } else if (petButton.tag == 3){
+        petFirstName = @"Maxi";
+        petLastName = @"Dog";
+        petPhoneNumber = @"5438880123";
+        petImageData = UIImageJPEGRepresentation([UIImage imageNamed:@"contact_Maxi.jpg"], 0.7f);
+    } else if (petButton.tag == 4){
+        petFirstName = @"Shippo";
+        petLastName = @"Dog";
+        petPhoneNumber = @"7124779070";
+        petImageData = UIImageJPEGRepresentation([UIImage imageNamed:@"contact_Shippo.jpg"], 0.7f);
+    }
+    ABAddressBookRef addressBookRef = ABAddressBookCreateWithOptions(NULL, nil);
+    ABRecordRef pet = ABPersonCreate();
+    ABRecordSetValue(pet, kABPersonFirstNameProperty, (__bridge CFStringRef)petFirstName, nil);
+    ABRecordSetValue(pet, kABPersonLastNameProperty, (__bridge CFStringRef)petLastName, nil);
+    ABMutableMultiValueRef phoneNumbers = ABMultiValueCreateMutable(kABMultiStringPropertyType);
+    
+    ABMultiValueAddValueAndLabel(phoneNumbers, (__bridge CFStringRef)petPhoneNumber, kABPersonPhoneMainLabel, NULL);
+    ABRecordSetValue(pet, kABPersonPhoneProperty, phoneNumbers, nil);
+    ABPersonSetImageData(pet, (__bridge CFDataRef)petImageData, nil);
+    ABAddressBookAddRecord(addressBookRef, pet, nil);
+    ABAddressBookSave(addressBookRef, nil);
+    
 }
 
 @end
